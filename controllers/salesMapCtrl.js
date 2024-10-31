59d8f2fefbab7960c1d43c81bec9bb53fe8aab62
hyeonji423 <hyeon423423@naver.com>
2024-10-31 15:51:15 +0900

    customers, visitors, salesmap data get

---
+++ controllers/salesMapCtrl.js
@@ -0,0 +1,11 @@
+const database = require('../database/db');
+
+exports.getSalesMap = async(req, res)=>{
+  try {
+    const result = await database.pool.query("SELECT * FROM sales_map")
+    return res.status(200).json(result.rows)
+  } catch (error) {
+    return res.status(500).json({msg: 'Gest sales_map Error : '+error})
+  }
+}
+
